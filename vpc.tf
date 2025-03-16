86ef4029248aca51f2120bfd470a3c0a715b9932
noctsol <39346808+Noctsol@users.noreply.github.com>
2025-03-15 23:24:12 -0700

    Update

--- vpc.tf
+++ vpc.tf
@@ -68,9 +68,9 @@ module "vpc_acct2west2" {
   tags = {
     Environment = "dev"
   }
-  # providers = {
-  #   aws = aws.acct2west2
-  # }
+  providers = {
+    aws = aws.acct2west2
+  }
 }
 
 resource "aws_subnet" "acct2west2" {
@@ -82,32 +82,6 @@ resource "aws_subnet" "acct2west2" {
     Environment = "dev"
   }
 }
-
-# resource "aws_vpc" "acct2west2" {
-#   provider   = aws.acct2west2
-#   cidr_block = "10.1.0.0/17"
-#   tags = {
-#     name = "vpcacct2west2"
-#     env  = "dev"
-#   }
-# }
-# resource "aws_subnet" "acct2west2" {
-#   provider   = aws.acct2west2
-#   vpc_id     = aws_vpc.acct2west2.id
-#   cidr_block = "10.1.0.0/22"
-#   tags = {
-#     name = "subnetacct2west2default"
-#     env  = "dev"
-#   }
-# }
-# resource "aws_route_table" "acct2west2" {
-#   provider = aws.acct2west2
-#   vpc_id = aws_vpc.acct2west2.id
-#   tags = {
-#     name = "routetableacct2west2"
-#     env  = "dev"
-#   }
-# }
 
 
 ###### PEERING VPCS ######
@@ -181,8 +155,8 @@ resource "aws_subnet" "acct2west2" {
 
 
 
-##### USING TRANSIT GATEWAY #####
-# 1. Create a transit gateway
+##### USING TRANSIT GATEWAY - SET UP 1 (ENABLED, ENABLED) #####
+# 1. Create a transit gateway and route table
 resource "aws_ec2_transit_gateway" "tgw" {
   description                     = "My Default Transit Gateway"
   amazon_side_asn                 = 64512 # Default ASN
@@ -194,8 +168,17 @@ resource "aws_ec2_transit_gateway" "tgw" {
     Environment = "prod"
   }
 }
+resource "aws_ec2_transit_gateway_route_table" "rootwest" {
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  tags = {
+    Name        = "tgwroutetablerootwest"
+    Environment = "prod"
+  }
+}
 
-# 2. Attach VPCs (technically subnets) to the transit gateway
+# 2. Attach VPCs to TGW via attachments
+#   - associate the route table with the attachment with association
+#   -  Add routes to the VPC route tables to rout through the TGW
 resource "aws_ec2_transit_gateway_vpc_attachment" "rootwest" {
   subnet_ids         = [aws_subnet.rootwest.id]
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
@@ -203,28 +186,57 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "rootwest" {
 }
 
 # Cross account works a little differently, you need to expose the TGW to the other accounts
-resource "aws_ram_resource_share" "tgw" {
-  name                      = "tgw"
+# Also need to expose the route table
+resource "aws_ram_resource_share" "rootwest" {
+  name                      = "rootwesttgwshare"
   allow_external_principals = false
   tags = {
     Name        = "tgwshare"
     Environment = "prod"
   }
 }
+
+# Associating the TGW with the resource share
 resource "aws_ram_resource_association" "tgw_association" {
-  resource_share_arn = aws_ram_resource_share.tgw.arn
+  resource_share_arn = aws_ram_resource_share.rootwest.arn
   resource_arn       = aws_ec2_transit_gateway.tgw.arn
 }
 
 resource "aws_ram_principal_association" "acct1west2" {
-  resource_share_arn = aws_ram_resource_share.tgw.arn
+  resource_share_arn = aws_ram_resource_share.rootwest.arn
   principal          = aws_organizations_account.testaccount1.id
 }
 
 resource "aws_ram_principal_association" "acct2west2" {
-  resource_share_arn = aws_ram_resource_share.tgw.arn
+  resource_share_arn = aws_ram_resource_share.rootwest.arn
   principal          = aws_organizations_account.testaccount2.id
 }
+
+# Associate the route table with the resource share
+resource "aws_ram_resource_share" "rootwesttgwroutetableshare" {
+  name                      = "rootwesttgwroutetableshare"
+  allow_external_principals = false
+  tags = {
+    Name        = "rootwesttgwroutetableshare"
+    Environment = "prod"
+  }
+}
+# resource "aws_ram_resource_association" "routetableshare" {
+#   resource_share_arn = aws_ram_resource_share.rootwesttgwroutetableshare.arn
+#   resource_arn       = aws_ec2_transit_gateway_route_table.rootwest.arn
+# }
+
+# resource "aws_ram_principal_association" "acct1west2routetable" {
+#   provider           = aws.acct1west2
+#   resource_share_arn = aws_ram_resource_share.rootwesttgwroutetableshare.arn
+#   principal          = aws_organizations_account.testaccount1.id
+# }
+# resource "aws_ram_principal_association" "acct2west2routetable" {
+#   provider           = aws.acct2west2
+#   resource_share_arn = aws_ram_resource_share.rootwesttgwroutetableshare.arn
+#   principal          = aws_organizations_account.testaccount2.id
+# }
+
 
 # Now you can attach the VPCs from other accounts as TGW will be shared
 resource "aws_ec2_transit_gateway_vpc_attachment" "acct1west2" {
@@ -232,20 +244,60 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "acct1west2" {
   subnet_ids         = [aws_subnet.acct1west2.id]
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   vpc_id             = aws_vpc.acct1west2.id
+  depends_on         = [aws_ram_principal_association.acct1west2]
 }
+# resource "aws_ec2_transit_gateway_route_table_association" "acct1west2" {
+#   provider                       = aws.acct1west2
+#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.acct1west2.id
+#   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.rootwest.id
+#   depends_on                     = [aws_ram_principal_association.acct1west2routetable]
+# }
+
 resource "aws_ec2_transit_gateway_vpc_attachment" "acct2west2" {
   provider           = aws.acct2west2
   subnet_ids         = [aws_subnet.acct2west2.id]
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   vpc_id             = module.vpc_acct2west2.vpc_id
+  depends_on         = [aws_ram_principal_association.acct2west2]
+}
+# resource "aws_ec2_transit_gateway_route_table_association" "acct2west2" {
+#   provider                       = aws.acct2west2
+#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.acct2west2.id
+#   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.rootwest.id
+#   depends_on                     = [aws_ram_principal_association.acct2west2routetable]
+# }
+
+# 3. Set VPCs route tables to route through the TGW
+resource "aws_route" "rootwest" {
+  route_table_id            = aws_route_table.rootwest.id
+  destination_cidr_block    = "0.0.0.0/0"
+  transit_gateway_id        = aws_ec2_transit_gateway.tgw.id
 }
 
-# 3. Create route tables
-# resource "aws_ec2_transit_gateway_route_table" "rootwest" {
-#   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-#   destination_cidr_block = aws_vpc.acct1west2.cidr_block
-#   tags = {
-#     Name = "routetablerootwest"
-#     Environment = "prod"
-#   }
-# }
+resource "aws_route" "acct1west2" {
+  provider                  = aws.acct1west2
+  route_table_id            = aws_route_table.acct1west2.id
+  destination_cidr_block    = "0.0.0.0/0"
+  transit_gateway_id        = aws_ec2_transit_gateway.tgw.id
+}
+
+resource "aws_route" "acct2west2" {
+  provider                  = aws.acct2west2
+  route_table_id            = module.vpc_acct2west2.route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  transit_gateway_id        = aws_ec2_transit_gateway.tgw.id
+}
+
+
+
+# In order
+# 1. Create TGW
+# 2. Create TGW route table
+# 3. Attach VPCs to TGW via attachments
+#   - associate the route table with the attachment with association
+#   -  Add routes to the VPC route tables
+#4. Add routes to the tgw route table
+# 5. Add routes to the VPC route tables
+
+
+########## USING TRANSIT GATEWAY - SET UP (DISABLED, DISABLED) ##########
